944f5615cbffbfcc5afe8d7886d447f85f6f58f0
user <user@192.168.10.148>
2015-10-28 10:59:16 +0100

    FIRST COMMIT
    Signed-off-by: user <user@192.168.10.148>

---
+++ greeter/src/main/java/edu/esprit/greeter/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author user
+ *
+ */
+package edu.esprit.greeter;
---
+++ greeter/src/test/java/edu/esprit/greeter/test/GreeterTest.java
@@ -0,0 +1,48 @@
+package edu.esprit.greeter.test;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.esprit.greeter.Greeter;
+
+
+public class GreeterTest {
+	Greeter greeter;
+	@Before
+	public void setUp(){
+		
+		 greeter=new Greeter();
+		
+		
+		
+	}
+	
+	
+	@Test
+	public void itShouldSayHelloId(){
+		
+		
+		 String result=greeter.sayHello("word");
+		 Assert.assertEquals("Hello word",result);
+		
+		
+	}
+	@Test(expected=IllegalArgumentException.class)
+	public void itShouldRaiseException(){
+		String param="MAN";
+		greeter.sayHello(param);
+		
+	}
+	
+	
+	@After
+	public void tearDown(){
+		
+		greeter=null;
+		
+	}
+
+}
